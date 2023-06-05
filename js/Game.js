b87046fe41559007085d8ea01a5265e1c1ab5de9
Danielle Sanches <93749543+DaniSanches@users.noreply.github.com>
2023-06-05 16:55:27 -0300

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,415 @@
+class Game {
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+
+    this.leadeboardTitle = createElement("h2");
+
+    this.leader1 = createElement("h2");
+    this.leader2 = createElement("h2");
+    this.playerMoving = false;
+    // 4º passo- criar a propriedade leftKeyActive(tecla esquerda ativa),
+    //  para mover o carro para  longe dos obstaculos assim que ele  colidir, a gente começa  com a  chave em  falso, 
+    // mas em caso de  colisão ela se  torna verdadeira e faz o carro saltar da para o lado  oposto da tecla pressionada
+    this.leftKeyActive = false;
+    // atividade 7- criar o propriedade para verificar o status da  explosão, ela  começa  como false, mas se a  vida do  player  for < 0 ela  será true
+    this.blast = false;
+  }
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("car1", car1_img);
+    car1.scale = 0.07;
+    // atividade 3- add a img da  explosão nas  sprites dos carros
+    car1.addImage("blast", blastImage);
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage("car2", car2_img);
+    car2.scale = 0.07;
+    // atividade 3- add a img da  explosão nas  sprites dos carros
+    car2.addImage("blast", blastImage);
+
+    cars = [car1, car2];
+
+    fuels = new Group();
+    powerCoins = new Group();
+
+    obstacles = new Group();
+
+    var obstaclesPositions = [
+      { x: width / 2 + 250, y: height - 800, image: obstacle2Image },
+      { x: width / 2 - 150, y: height - 1300, image: obstacle1Image },
+      { x: width / 2 + 250, y: height - 1800, image: obstacle1Image },
+      { x: width / 2 - 180, y: height - 2300, image: obstacle2Image },
+      { x: width / 2, y: height - 2800, image: obstacle2Image },
+      { x: width / 2 - 180, y: height - 3300, image: obstacle1Image },
+      { x: width / 2 + 180, y: height - 3300, image: obstacle2Image },
+      { x: width / 2 + 250, y: height - 3800, image: obstacle2Image },
+      { x: width / 2 - 150, y: height - 4300, image: obstacle1Image },
+      { x: width / 2 + 250, y: height - 4800, image: obstacle2Image },
+      { x: width / 2, y: height - 5300, image: obstacle1Image },
+      { x: width / 2 - 180, y: height - 5500, image: obstacle2Image }
+    ];
+
+    
+    this.addSprites(fuels, 4, fuelImage, 0.02);
+
+    this.addSprites(powerCoins, 18, powerCoinImage, 0.09);
+
+  
+    this.addSprites(
+      obstacles,
+      obstaclesPositions.length,
+      obstacle1Image,
+      0.04,
+      obstaclesPositions
+    );
+  }
+
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale, positions = []) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+      if (positions.length > 0) {
+        x = positions[i].x;
+        y = positions[i].y;
+        spriteImage = positions[i].image;
+      } else {
+        x = random(width / 2 + 150, width / 2 - 150);
+        y = random(-height * 4.5, height - 400);
+      }
+      var sprite = createSprite(x, y);
+      sprite.addImage("sprite", spriteImage);
+
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+
+ 
+    this.resetTitle.html("Reset Game");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width / 2 + 200, 40);
+
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 230, 100);
+
+    this.leadeboardTitle.html("Leaderboard");
+    this.leadeboardTitle.class("resetText");
+    this.leadeboardTitle.position(width / 3 - 60, 40);
+
+    this.leader1.class("leadersText");
+    this.leader1.position(width / 3 - 50, 80);
+
+    this.leader2.class("leadersText");
+    this.leader2.position(width / 3 - 50, 130);
+  }
+
+  play() {
+    this.handleElements();
+    this.handleResetButton();
+
+    Player.getPlayersInfo();
+    player.getCarsAtEnd();
+
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      this.showFuelBar();
+      this.showLife();
+      this.showLeaderboard();
+
+     
+      var index = 0;
+      for (var plr in allPlayers) {
+        index = index + 1;
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
+        //atividade 4- salvar o   valor de player. life na  variável temporaria de currentLife
+        var currentlife = allPlayers[plr].life;
+
+        if (currentlife <= 0) {
+          cars[index - 1].changeImage("blast");
+          cars[index - 1].scale = 0.3;
+        }
+
+        cars[index - 1].position.x = x;
+        cars[index - 1].position.y = y;
+
+        if (index === player.index) {
+          stroke(10);
+          fill("red");
+          ellipse(x, y, 60, 60);
+
+          this.handleFuel(index);
+          this.handlePowerCoins(index);
+          // atividade 6- chamar a  função para  verificar se os  carros colidiram
+          this.handleCarACollisionWithCarB(index);
+          // 2º passo chame a função
+          this.handleObstacleCollision(index);
+
+          if (player.life <= 0) {
+            // atividade 8-  chamar a classe blast se  a  vida for <= a 0
+            this.blast = true;
+            this.playerMoving = false;
+          }
+
+          
+          camera.position.y = cars[index - 1].position.y;
+        }
+      }
+
+      if (this.playerMoving) {
+        player.positionY += 5;
+        player.update();
+      }
+
+      
+      this.handlePlayerControls();
+
+    
+      const finshLine = height * 6 - 100;
+
+      if (player.positionY > finshLine) {
+        gameState = 2;
+        player.rank += 1;
+        Player.updateCarsAtEnd(player.rank);
+        player.update();
+        this.showRank();
+      }
+
+      drawSprites();
+    }
+  }
+
+  handleResetButton() {
+    this.resetButton.mousePressed(() => {
+      database.ref("/").set({
+        playerCount: 0,
+        gameState: 0,
+        players: {},
+        carsAtEnd: 0
+      });
+      window.location.reload();
+    });
+  }
+
+  showLife() {
+    push();
+    image(lifeImage, width / 2 - 130, height - player.positionY - 400, 20, 20);
+    fill("white");
+    rect(width / 2 - 100, height - player.positionY - 400, 185, 20);
+    fill("#f50057");
+    rect(width / 2 - 100, height - player.positionY - 400, player.life, 20);
+    noStroke();
+    pop();
+  }
+
+  showFuelBar() {
+    push();
+    image(fuelImage, width / 2 - 130, height - player.positionY - 350, 20, 20);
+    fill("white");
+    rect(width / 2 - 100, height - player.positionY - 350, 185, 20);
+    fill("#ffc400");
+    rect(width / 2 - 100, height - player.positionY - 350, player.fuel, 20);
+    noStroke();
+    pop();
+  }
+
+  showLeaderboard() {
+    var leader1, leader2;
+    var players = Object.values(allPlayers);
+    if (
+      (players[0].rank === 0 && players[1].rank === 0) ||
+      players[0].rank === 1
+    ) {
+      leader1 =
+        players[0].rank +
+        "&emsp;" +
+        players[0].name +
+        "&emsp;" +
+        players[0].score;
+
+      leader2 =
+        players[1].rank +
+        "&emsp;" +
+        players[1].name +
+        "&emsp;" +
+        players[1].score;
+    }
+
+    if (players[1].rank === 1) {
+      leader1 =
+        players[1].rank +
+        "&emsp;" +
+        players[1].name +
+        "&emsp;" +
+        players[1].score;
+
+      leader2 =
+        players[0].rank +
+        "&emsp;" +
+        players[0].name +
+        "&emsp;" +
+        players[0].score;
+    }
+
+    this.leader1.html(leader1);
+    this.leader2.html(leader2);
+  }
+
+  handlePlayerControls() {
+    // atividade 9 -  vamos  fazer o os controles  funcionarem de o this.blast for  false
+    if (!this.blast) {
+      if (keyIsDown(UP_ARROW)) {
+        this.playerMoving = true;
+        player.positionY += 10;
+        player.update();
+      }
+
+      if (keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50) {
+        // 5º passo - para descobrir  qual seta o usuário ciclou será  true se for left
+        this.leftKeyActive = true;
+        player.positionX -= 5;
+        player.update();
+      }
+
+      if (keyIsDown(RIGHT_ARROW) && player.positionX < width / 2 + 300) {
+        // 5º passo - para descobrir  qual seta o usuário ciclou será  false se for right
+        this.leftKeyActive = false;
+        player.positionX += 5;
+        player.update();
+      }
+    }
+  }
+
+  handleFuel(index) {
+    cars[index - 1].overlap(fuels, function(collector, collected) {
+      player.fuel = 185;
+      collected.remove();
+    });
+
+    
+    if (player.fuel > 0 && this.playerMoving) {
+      player.fuel -= 0.3;
+    }
+
+    if (player.fuel <= 0) {
+      gameState = 2;
+      this.gameOver();
+    }
+  }
+
+  handlePowerCoins(index) {
+    cars[index - 1].overlap(powerCoins, function(collector, collected) {
+      player.score += 21;
+      player.update();
+      collected.remove();
+    });
+  }
+
+  // 1º passo // Crie handleObstcaleCollision() em game.js com uma condição if para usar collide() entre carros e obstáculos.
+  handleObstacleCollision(index) {
+    if (cars[index - 1].collide(obstacles)) {
+      // 6º- passo para mover o carro para longe dos obstáculos após uma colisão com base na tecla de seta pressionada pelo jogador
+      if (this.leftKeyActive) {
+        player.positionX += 100;
+      } else {
+        player.positionX -= 100;
+      }
+
+      //Reduzindo a vida do jogador
+      if (player.life > 0) {
+        player.life -= 185 / 4;
+      }
+
+      player.update();
+    }
+  }
+  // atividade 5- cirar uma função  para verficar se  um  carros  bate no outro
+  handleCarACollisionWithCarB(index) {
+    if (index === 1) {
+      if (cars[index - 1].collide(cars[1])) {
+        if (this.leftKeyActive) {
+          player.positionX += 100;
+        } else {
+          player.positionX -= 100;
+        }
+
+        //Reduzindo a vida do jogador
+        if (player.life > 0) {
+          player.life -= 185 / 4;
+        }
+
+        player.update();
+      }
+    }
+    if (index === 2) {
+      if (cars[index - 1].collide(cars[0])) {
+        if (this.leftKeyActive) {
+          player.positionX += 100;
+        } else {
+          player.positionX -= 100;
+        }
+
+        //Reduzindo a vida do jogador
+        if (player.life > 0) {
+          player.life -= 185 / 4;
+        }
+
+        player.update();
+      }
+    }
+  }
+
+  showRank() {
+    swal({
+      title: `Incrível!${"\n"}Rank${"\n"}${player.rank}`,
+      text: "Você alcançou a linha de chegada com sucesso!",
+      imageUrl:
+        "https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png",
+      imageSize: "100x100",
+      confirmButtonText: "Ok"
+    });
+  }
+
+  gameOver() {
+    swal({
+      title: `Fim de Jogo`,
+      text: "Oops você perdeu a corrida!",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_grande.png",
+      imageSize: "100x100",
+      confirmButtonText: "Obrigado por jogar"
+    });
+  }
+  
+  // atiivdade10 criar um  metodo end para  finalizar o jogo caso  o gameState seja 2
+  
+  end() {
+    console.log("Fim de Jogo");
+  }
+}
